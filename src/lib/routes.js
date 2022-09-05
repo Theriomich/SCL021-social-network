7f43ac29fabf761ae9eedf09bd2483698361ee24
BEMM12 <bemm1984@gmail.com>
2022-09-05 18:47:33 -0400

    Estructura del welcome Co-authored-by: nightmich@hotmail.com <nightmich@hotmail.com>

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -1,22 +1,26 @@
-/* eslint-disable import/no-unresolved */
-// eslint-disable-next-line import/no-unresolved
-import { welcome } from './welcome.js';
-import { login } from './login.js';
-import { register } from './register.js';
-import { wall } from './wall.js';
+//import { welcome } from '../views/welcome.js';
+import { login } from '../views/login.js';
+import { register } from '../views/register.js';
+import { wall } from '../views/wall.js';//
 
 export const routes = (hash) => {
   const menuRegister = document.getElementById('root');
   menuRegister.innerHTML = '';
-  if (hash === '#/' || hash === '/' || hash === '#' || hash === '') {
-    menuRegister.appendChild(welcome());
-  } else if (hash === '#/welcome') {
-    menuRegister.appendChild(welcome());
-  } else if (hash === '#/login') { 
-    menuRegister.appendChild(login());
-  } else if (hash === '#/register') {
-    menuRegister.appendChild(register());
-  } else if (hash === '#/wall') {
-    menuRegister.appendChild(wall());
+  switch (hash) {
+    case '#/':
+    case '#/welcome':
+      menuRegister.appendChild(welcome());
+      break;
+    case '#/login':
+      menuRegister.appendChild(login());
+      break;
+    case '#/register':
+      menuRegister.appendChild(register());
+      break;
+   // case '#/wall':
+   //   menuRegister.appendChild(wall());
+   //   break;//
+    default:
+      break;
   }
 };
